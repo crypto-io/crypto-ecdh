d55b7def12aab40ef82b500ad67afbd9a5f12068
vandeurenglenn <vandeurenglenn@gmail.com>
2020-09-10 22:44:33 +0200

    Simplify Api
    - remove lock & unlock
    - remove store
    - fix encoding param

--- src/index.js
+++ src/index.js
@@ -2,27 +2,15 @@ import { encrypt, decrypt } from './../node_modules/crypto-io-utils/dist/utils-e
 import crypto from 'crypto';
 
 export default (encoding = 'hex') => {
-  const store = {};
+  let secret;
   // Generate keys
   const ecdh = crypto.createECDH('secp256k1');
   const pair = ecdh.generateKeys(encoding);
   return {
-    derive: pub => store.secret = ecdh.computeSecret(pub, encoding),
+    derive: pub => secret = ecdh.computeSecret(pub, encoding).toString(encoding),
     public: ecdh.getPublicKey(encoding, 'compressed'),
     private: ecdh.getPrivateKey(encoding, 'compressed'),
-    encrypt: data => encrypt(data, store.secret.toString('hex')),
-    decrypt: data => {
-      if (!data) {
-        // try reading from our stored values when data is not defined
-        data = store.data;
-        if (!data) {
-          return 'error: data undefined'
-        }
-      }
-      // decrypt given or stored data
-      return decrypt(data, store.secret.toString('hex'))
-    },
-    lock: data => encrypt(data, store.secret.toString('hex')).then(data => store.data = data),
-    unlock: data => decrypt(store.data, store.secret.toString('hex')),
+    encrypt: data => encrypt(data, secret),
+    decrypt: data => decrypt(data, secret)
   }
 }
