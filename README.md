c550803aa685a305c1f4a2840fba0eb67c1bef86
vandeurenglenn <vandeurenglenn@gmail.com>
2020-11-22 17:41:53 +0100

    Fix example

--- README.md
+++ README.md
@@ -16,11 +16,11 @@
 const client = ecdh();
 const server = ecdh();
 
-const sharedClient = client.derive(pair.public);
-const sharedServer = pair.derive(client.public);
+const sharedClient = client.derive(server.public);
+const sharedServer = server.derive(client.public);
 
 client.encrypt('this is a test')
-  .then(cipher => pair.decrypt(cipher))
+  .then(cipher => server.decrypt(cipher))
   .then(data => tape.equal(data, 'this is a test', 'encrypt/decrypt'))
 
 ```
--- test.js
+++ test.js
@@ -3,13 +3,13 @@ const ecdh = require('./ecdh.js');
 
 test('ready', tape => {
   tape.plan(3);
-  const pair = ecdh();
+  const server = ecdh();
   const client = ecdh();
-  const sharedClient = client.derive(pair.public);
-  const sharedServer = pair.derive(client.public);
+  const sharedClient = client.derive(server.public);
+  const sharedServer = server.derive(client.public);
   tape.equal(sharedClient.toString('hex'), sharedServer.toString('hex'), 'shared secret');
   tape.notEqual(client.private, undefined, 'private key');
   client.encrypt('this is a test')
-    .then(cipher => pair.decrypt(cipher))
+    .then(cipher => server.decrypt(cipher))
     .then(data => tape.equal(data, 'this is a test', 'encrypt/decrypt'))
 })
